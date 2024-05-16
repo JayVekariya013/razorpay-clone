5ab3eb7f8379d997dc3a65ea08a87c6fa749529f
Jay Vekariya <jayvekariya013@gmail.com>
2024-05-17 00:00:07 +0530

    Day 1: navbar
    In the first day I have created a navbar of the Razorpay clone site. In this site I am using Tailwind for designing.

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        gray2: "#525a76",
+        grayText: "#818597",
+        greenLight: "#61cea6",
+        grayBlue: "#344a6c",
+        lightGray: "#e2e2e2",
+        deepBlueHead: "#162f56",
+      },
+    },
+  },
+  plugins: [],
+}
+
